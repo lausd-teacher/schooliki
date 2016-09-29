1e5aafd48621ea15de3b7c8ee4cca7e13c6389a4
Lee ViDemantay <tdd6623@lausd.net>
2016-09-28 18:58:05 -0700

    hibernate validator workaround hibernate 5.3 cr1

--- src/net/videmantay/server/validation/ValidDateRange.java
+++ src/net/videmantay/server/validation/ValidDateRange.java
@@ -7,11 +7,19 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 import javax.validation.Constraint;
+import javax.validation.Payload;
 
 @Target({ ElementType.TYPE, ElementType.ANNOTATION_TYPE })
 @Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy = { ValidDateRangeValidator.class })
 @Documented
 public @interface ValidDateRange {
+	
+	String message() default "Invalid date range";
 
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+	
 }
