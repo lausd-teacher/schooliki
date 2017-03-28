b7abf916453ae69e012cc30df3312b1623158521
Lee ViDemantay <tdd6623@lausd.net>
2017-03-28 00:24:02 -0700

    Gradebook improvements

--- DrSammyLee/src/net/videmantay/roster/views/assignment/AssignmentMain.java
+++ DrSammyLee/src/net/videmantay/roster/views/assignment/AssignmentMain.java
@@ -3,15 +3,19 @@ package net.videmantay.roster.views.assignment;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
+import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.AbsolutePanel;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.view.client.AsyncDataProvider;
 import com.google.gwt.view.client.HasData;
-
+import static com.google.gwt.query.client.GQuery.*;
 import gwt.material.design.client.ui.MaterialAnchorButton;
+import gwt.material.design.client.ui.MaterialSwitch;
 import net.videmantay.roster.RosterUtils;
 import net.videmantay.roster.json.GradedWorkJson;
 
@@ -50,12 +54,31 @@ public class AssignmentMain extends Composite {
 		
 	@UiField(provided=true)
 	public final AssignmentGrid assignmentGrid = new AssignmentGrid(providesKey);
+	
+	@UiField
+	public MaterialSwitch gradebookSwitch;
+	
+	@UiField
+	public AbsolutePanel assignmentContent;
 
 	public AssignmentMain(RosterUtils ru) {
 		utils =ru;
 		gradedworkForm = new GradedWorkForm(utils);
 		initWidget(uiBinder.createAndBindUi(this));
 		fabBtn.addClickHandler(fabHandler);
+		
+		//add value change handler to switch
+		gradebookSwitch.addValueChangeHandler(new ValueChangeHandler<Boolean>(){
+
+			@Override
+			public void onValueChange(ValueChangeEvent<Boolean> event) {
+				if(event.getValue()){
+					assignmentContent.clear();
+					assignmentContent.add(new GradebookPanel());
+					$(body).css("overflow", "hidden");
+				}
+				
+			}});
 		
 	}
 
--- DrSammyLee/src/net/videmantay/roster/views/assignment/GradebookPanel.java
+++ DrSammyLee/src/net/videmantay/roster/views/assignment/GradebookPanel.java
@@ -4,6 +4,8 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
+import static com.google.gwt.query.client.GQuery.*;
+import com.google.gwt.query.client.Function;
 
 public class GradebookPanel extends Composite {
 
@@ -15,5 +17,27 @@ public class GradebookPanel extends Composite {
 	public GradebookPanel() {
 		initWidget(uiBinder.createAndBindUi(this));
 	}
+	@Override
+	public void onLoad(){
+		
+			/*$(window).scroll(new Function() {
+				
+			@Override
+			public void f(){
+				$(".assignment-title").css("top",$(this).scrollTop()+"px");
+					
+			}
+			});*/
+			
+			$("section#gradebookSection").scroll(new Function() {
+				
+			@Override
+			public void f(){
+				$(".assignment-title").css("top",$(this).scrollTop()+"px");
+					
+			}
+			});
+	}
+	
 
 }
